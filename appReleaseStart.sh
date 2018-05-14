e81280e7f595c4f6e942182dda3314802a29f6fb
TFsky <TFsky@huangwenchundeMacBook-Pro.local>
2018-05-14 16:36:07 +0800

    android打包相关命令&&渠道号

---
+++ appReleaseStart.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#fix RN0.45bug Could not list contents of '.........../node_modules/react-native/third-party/glog-0.3.4/test-driver'. Couldn't follow symbolic link.
+#https://github.com/facebook/react-native/issues/14464
+if [ -L "node_modules/react-native/third-party/glog-0.3.4/test-driver" ]; then
+    unlink "node_modules/react-native/third-party/glog-0.3.4/test-driver"
+fi
+
+react-native run-android --variant devRelease
+
+
